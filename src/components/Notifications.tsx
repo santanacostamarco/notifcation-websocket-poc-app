5c3d8715b79dd548955465da5bd77e601d4df0f2
Marco Antonio Costa Santana <marcosantana@cvccorp.com.br>
2023-10-05 12:39:57 -0300

    notifications

--- src/components/Notifications.tsx
+++ src/components/Notifications.tsx
@@ -1,55 +1,120 @@
 "use client";
 
-import { useEffect, useState } from 'react';
+import { useCallback, useEffect, useState } from 'react';
 import io from 'socket.io-client'
 import icon from '@/app/favicon.ico'
 
+type NotificationProps = {
+  id: string;
+  date: Date;
+  message: string;
+  read: boolean
+}
+
 const Notifications = () => {
-  const [hasBrowserNotification, setHasBrowserNotification] = useState(false)
+  const [notificationPermitionGranted, setNotifictionPermitionGranted] = useState(false)
+  const [notificationHistory, setNotificationHistory] = useState<NotificationProps[]>([])
+
+  const pushNotification = useCallback((notification: NotificationProps) => {
+    setNotificationHistory(notificationHistory => [...notificationHistory, notification])
+  }, [])
+
+  const setRead = useCallback((notification: NotificationProps) => {
+    setNotificationHistory(notificationHistory => notificationHistory.map(_notification => {
+      if (notification.id === _notification.id) {
+        return {
+          ..._notification, 
+          read: true
+        }
+      } 
+      return _notification
+    }))
+  }, [])
   
   useEffect(() => {
     if (!("Notification" in window)) {
       console.log("Este navegador não suporta notificações.");
     }
     else {
-      setHasBrowserNotification(true)
+      Notification.requestPermission().then((permission) => {
+        if (permission === "granted") {
+          setNotifictionPermitionGranted(true)
+        } else {
+          setNotifictionPermitionGranted(false)
+        }
+      });
     }
   }, [])
     
   useEffect(() => {
-    Notification.requestPermission().then((permission) => {
-      if (permission === "granted") {
-        console.log("Permissão concedida para exibir notificações.");
-      } else {
-        console.log("Permissão negada para exibir notificações.");
-      }
-    });
-    
     const socket = io('http://localhost:5000');
 
-    socket.on('notification', (data: any) => {
-      console.log('Received notification:', data);
+    socket.on('notification', (notification: Omit<NotificationProps, 'read'>) => {
+      pushNotification({
+        ...notification,
+        read: false
+      })
 
-      const options = {
-        body: data.message,
-        icon: icon.src,
-      };
-    
-      const notification = new Notification("Título da Notificação", options);
-    
-      notification.onclick = () => {
-        console.log("A notificação foi clicada!");
-      };
+      if (notificationPermitionGranted) {
+        const windowNotification = new Notification(
+          "Nova notificação",  
+          {
+            body: notification.message,
+            icon: icon.src,
+            data: {
+              ...notification
+            },
+          }
+        );
+      
+        windowNotification.onclick = (event) => {
+          const notification = event.currentTarget as Notification
+          const notificationData = notification.data as NotificationProps
+
+          setRead(notificationData)
+        };
+      }
 
     });
 
     return () => {
       socket.disconnect();
     };
-  }, []);
+  }, [notificationPermitionGranted, pushNotification, setRead]);
 
   return (
-  <p> Notification </p>
+    <div> 
+      <h1> Notificações </h1>
+      {notificationPermitionGranted && <p>
+        Permissão concedida para exibir notificações.
+      </p>}
+      {!notificationPermitionGranted && <div>
+        <p>
+        Permissão negada para exibir notificações.
+        </p>
+        <button>
+          Permitir notificações
+        </button>
+      </div>}
+      <ul>
+        {notificationHistory.map(notification => (
+          <li key={notification.id}>
+            {notification.message}
+            {notification.read ? " - lida" : ""}
+            {!notification.read ? (
+              <> {' '}
+              <button onClick={() => {
+                setRead(notification)
+              }}>
+                Marcar como Lida
+              </button>
+              </>
+            ) : null}
+          </li>
+        ))}
+      </ul>
+    </div>
+  
   )
 }
 
